92a48f33cdbecc7e20272d850276dd9e04ae9568
liusujin <1435679490@qq.com>
2019-12-30 13:35:24 +0800

    添加激活失活接口

---
+++ Assets/com.et.module.interface/Runtime/IActivate.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace ETModel
+{
+    /// <summary>
+    /// 激活接口
+    /// </summary>
+    public interface IActivate
+    {
+        bool Activate();
+        Task<bool> ActivateAsync();
+    }
+}
