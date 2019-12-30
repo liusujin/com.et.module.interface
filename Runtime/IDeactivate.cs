3dc0832a58dae32c419c0aec5aa359f127ac1350
liusujin <1435679490@qq.com>
2019-12-30 13:35:24 +0800

    添加激活失活接口

---
+++ Runtime/IDeactivate.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace ETModel
+{
+    /// <summary>
+    /// 失活接口
+    /// </summary>
+    public interface IDeactivate
+    {
+        bool Deactivate();
+        Task<bool> DeactivateAsync();
+    }
+}
